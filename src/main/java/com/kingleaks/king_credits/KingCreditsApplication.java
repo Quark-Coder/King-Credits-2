a9a225859ce218d245d8ffcfcea5f49024f9e9c2
nakermaz <telvintamer@gmail.com>
2024-08-06 12:43:21 +0500

    Добавил карткас проекта

---
+++ src/main/java/com/kingleaks/king_credits/KingCreditsApplication.java
@@ -0,0 +1,13 @@
+package com.kingleaks.king_credits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingCreditsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KingCreditsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kingleaks/king_credits/KingCreditsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kingleaks.king_credits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KingCreditsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
